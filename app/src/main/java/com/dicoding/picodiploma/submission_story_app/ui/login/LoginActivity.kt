195f358e7e4b950365b81539e5c5e113b5a12842
aditya-radiant <77778937+aditya-radiant@users.noreply.github.com>
2022-04-20 15:06:53 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/picodiploma/submission_story_app/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.picodiploma.submission_story_app.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.picodiploma.submission_story_app.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
